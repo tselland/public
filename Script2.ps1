b9d15f9fc728a7568b8b3d38ea2733b2a32d3cd8
Carlos <filoxo@users.noreply.github.com>
2014-11-21 10:55:58 -0700

    Removed script1

--- Script2.ps1
+++ Script2.ps1
@@ -3,49 +3,6 @@
 ## Group 2B
 ## Carlos Filoteo, Nathan Dudley, Michael Helvey, Travis Selland
 
-## ----- Script 1 - Create Active Directory User Accounts
-
-#parameters
-
-param([string]$csv_source="c:\is531\public\GroupProject3Usernames.csv")
-
-# accepts a csv parameter
-if ($csv_source) {
-    $source_exists = Test-Path $csv_source -PathType Leaf
-
-    if($source_exists){
-        $names = Import-Csv $csv_source
-    } else {
-        Write-Host "ERROR: Source ($csv_source) does not exist or is a directory" -ForegroundColor Red
-        break
-    }
-} else {
-    Write-Host "ERROR: Source is not defined" -ForegroundColor Red
-    break
-}
-#loop for each row
-#retrieve params from imported csv
-
-$csv_source
-$names
-
-
-Foreach($line in $names){
-    $username = $line.username
-    $surname = $line.lastname
-    $givenname = $line.firstname
-
-    New-ADUser $username
-    $user = Get-ADUser $username
-    $user.Surname = $surname
-    $user.Givenname = $givenname
-
-    Set-ADUser -instance $user
-}
-
-
-
-
 ## ----- Script 2 - Gather information about Windows computers in your enterprise
 
 ##NEEDSWORK: fix method to get computerName or list of computers.
