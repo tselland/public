4ea24ee028ac915eba288c76326893561531795d
Nathan Dudley <dudleynathan@byu.net>
2014-11-20 23:43:07 -0700

    Changed display to custom table, fixed lastbootuptime, added Set-Stats Function

--- 531 Project 3 - Powershell Admin Script.ps1
+++ 531 Project 3 - Powershell Admin Script.ps1
@@ -51,32 +51,55 @@ Foreach($line in $names){
 ##NEEDSWORK: fix method to get computerName or list of computers.
 param([string]$computer = (Get-WmiObject -Class Win32_Desktop -ComputerName))
 
+Function Set-Stats([string]$label, [string]$info) {
+    $stats = "" | Select Label, Info
+    $stats.label = $label
+    $stats.info = $info
+
+    return $stats
+}
+
 $computerSystem = Get-WmiObject Win32_ComputerSystem
 $computerBIOS = Get-CimInstance CIM_BIOSElement
 $computerOS = Get-WmiObject Win32_OperatingSystem
 $computerCPU = Get-WmiObject Win32_Processor
 $computerHDD = Get-WMIObject Win32_LogicalDisk -Filter "DeviceID = 'C:'"
-$computerBattery = Get-WmiObject Win32_battery
+$computerBattery = Get-WmiObject Win32_Battery
+$lastBootUpTime = Get-CimInstance -ClassName win32_operatingsystem | select lastbootuptime
 Clear-Host
 
+$table = @()
+
 Write-Host "System Information for: " $computerSystem.Name -BackgroundColor DarkCyan
-"Manufacturer: " + $computerSystem.Manufacturer
-"Model: " + $computerSystem.Model
-"Serial Number: " + $computerBIOS.SerialNumber
-"CPU: " + $computerCPU.Name
-"HDD Capacity: "  + "{0:N2}" -f ($computerHDD.Size/1GB) + "GB"
-"HDD Space: " + "{0:P2}" -f ($computerHDD.FreeSpace/$computerHDD.Size) + " Free (" + "{0:N2}" -f ($computerHDD.FreeSpace/1GB) + "GB)"
-"RAM: " + "{0:N2}" -f ($computerSystem.TotalPhysicalMemory/1GB) + "GB"
-"Operating System: " + $computerOS.caption + ", Service Pack: " + $computerOS.ServicePackMajorVersion
-"User logged In: " + $computerSystem.UserName
-"Last Reboot: " + $computerOS.LastBootUpTime
+$table += Set-Stats "Manufacturer" $computerSystem.Manufacturer
+$table += Set-Stats "Model" $computerSystem.Model
+$table += Set-Stats "Serial Number" $computerBIOS.SerialNumber
+$table += Set-Stats "CPU" $computerCPU.Name
+
+$hddCap = "{0:N2}" -f ($computerHDD.Size/1GB) + "GB"
+$table += Set-Stats "HDD Capacity" $hddCap
+
+$hddSpace = "{0:P2}" -f ($computerHDD.FreeSpace/$computerHDD.Size) + " Free (" + "{0:N2}" -f ($computerHDD.FreeSpace/1GB) + "GB)"
+$table += Set-Stats "HDD Space" $hddSpace
+
+$ram = "{0:N2}" -f ($computerSystem.TotalPhysicalMemory/1GB) + "GB"
+$table += Set-Stats "RAM" $ram
+
+$table += Set-Stats "Operating System" $computerOS.caption
+$table += Set-Stats "Service Pack" $computerOS.ServicePackMajorVersion
+$table += Set-Stats "Current User" $computerSystem.UserName
+$table += Set-Stats "Last Reboot" $lastBootUpTime.lastbootuptime
 
 if($computerBattery) {
     $percentRemaining = $computerBattery.EstimatedChargeRemaining
     $hoursRemaining = [Math]::Floor([decimal]($computerBattery.EstimatedRunTime / 60))
     $minutesRemaining = $computerBattery.EstimatedRunTime % 60
- 
-    "Battery Remaining: $percentRemaining% - $hoursRemaining hours $minutesRemaining minutes remaining"
+    
+    $timeRemaining = "$percentRemaining% ($hoursRemaining hours $minutesRemaining minutes remaining)"
+    $table+= Set-Stats "Battery Remaining" $timeRemaining
+
 } else {
-    "Battery Status: No battery connected"
+    $table += Set-Stats "Battery Remaining" "No battery connected"
 }
+
+$table
