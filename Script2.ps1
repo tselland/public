f16cceffeabfcffbb3ca1678d1d1fa735ced8841
nathandud <dudleynathan@byu.net>
2014-11-24 13:10:56 -0700

    Update Script2.ps1

--- Script2.ps1
+++ Script2.ps1
@@ -33,10 +33,10 @@ $lastBootUpTime = Get-CimInstance -ComputerName $computer_name -ClassName win32_
 $userSession = Get-WmiObject Win32_Session -ComputerName $computer_name
 Clear-Host
 
-#Create table object that will be populated with system information.
+#Create table object that will be populated with system information
 $table = @()
 
-#Gather basic information pertaining to Hard Drive, RAM, and Session time.
+#Gather basic information pertaining to Hard Drive, RAM, and Session time
 $hddCap = "{0:N2}" -f ($computerHDD.Size/1GB) + "GB"
 $hddSpace = "{0:P2}" -f ($computerHDD.FreeSpace/$computerHDD.Size) + " Free (" + "{0:N2}" -f ($computerHDD.FreeSpace/1GB) + "GB)"
 $ram = "{0:N2}" -f ($computerSystem.TotalPhysicalMemory/1GB) + "GB"
@@ -116,31 +116,33 @@ if ($no_report -eq $false) {
     $ws.Cells.Item(1,"a").EntireRow.Delete() | Out-Null
 
     #Add Sub Headings
+    #These headings are hard coded based on a fixed format
     $headingRows = @(1, 6, 14, 18, 23)
     $headingCaptions = @("Computer Specs", "Hardware", "Operating System", "User and Session", "Battery Life")
     $counter = 0
 
     #for each heading row, change color and formatting
     foreach($hr in $headingRows){
-        $ws.Range("a$hr").EntireRow.Insert(-4142) | Out-Null
+        $ws.Range("a$hr").EntireRow.Insert(-4142) | Out-Null #space out the different sections
         $ws.Range("a$hr").EntireRow.Insert(-4142) | Out-Null
         $hr+=1
         $range = "a$hr" + ":b$hr"
-        $ws.Range($range).Merge() | Out-Null
-        $ws.Cells.Item($hr, "a").Interior.ColorIndex = 44
-        $ws.Cells.Item($hr,"a").HorizontalAlignment = -4108
-        $ws.Cells.Item($hr, "a") = $headingCaptions[$counter]
+        $ws.Range($range).Merge() | Out-Null #merge the heading cells
+        $ws.Cells.Item($hr, "a").Interior.ColorIndex = 44 #Orange interior
+        $ws.Cells.Item($hr,"a").HorizontalAlignment = -4108 #Centered
+        $ws.Cells.Item($hr, "a") = $headingCaptions[$counter] #Add the caption
         $counter++
     }
-
+    
+    #Format each table with borders and 
     foreach($hr in $headingRows){
-        $hr+=2
+        $hr+=2 #We're offset by two because of the rows added in the previous loop
         $dataRange = $ws.Cells.Item($hr, "a").CurrentRegion
-        $dataRange.Borders.LineStyle = 1
-        $dataRange.Borders.Weight = 2
-        $dataRange.HorizontalAlignment = -4131
+        $dataRange.Borders.LineStyle = 1 #Straight line
+        $dataRange.Borders.Weight = 2 #Typical weight, normal - not thick
+        $dataRange.HorizontalAlignment = -4131 #Left
         $hr-=1
-        $ws.Cells.Item($hr,"a").HorizontalAlignment = -4108
+        $ws.Cells.Item($hr,"a").HorizontalAlignment = -4108 #Centered
     }
 
     #Add Primary Heading
@@ -148,31 +150,32 @@ if ($no_report -eq $false) {
     $ws.Cells.Item(1,"a") = "Summary Data for $($computerSystem.Name)"
     $ws.Cells.Item(1,1).Font.Size = 14
     $ws.Cells.Item(1,1).Font.Bold = $True
-    $ws.Cells.Item(1,"a").Interior.ColorIndex = 6
+    $ws.Cells.Item(1,"a").Interior.ColorIndex = 6 #Yellow
     $ws.Range("a1:b1").Merge() | Out-Null
     $ws.Columns.Autofit() | Out-Null
-    $ws.Cells.Item(1,1).HorizontalAlignment = -4108 
+    $ws.Cells.Item(1,1).HorizontalAlignment = -4108 #Center
 
+    #Create another small table with raw data and header for the charts
     $ws.Cells.Item(3, "f") = "Free"
     $ws.Cells.Item(4, "f") = $computerHDD.FreeSpace/1GB
     $ws.Cells.Item(3, "g") = "Used"
     $ws.Cells.Item(4, "g") = $computerHDD.Size/1GB - $computerHDD.FreeSpace/1GB
-
-    $ws.Range("f3:g4").Select() | Out-Null
-
+    
+    #Create an empty chart
     $chart = $ws.Shapes.AddChart().Chart
-    #[Enum]::getvalues([Microsoft.Office.Interop.Excel.XlChartType]) | select @{n="Name";e={"$_"}},value__ | ft -auto
-    $chart.ChartType = 5
-
+    $chart.ChartType = 5 #pie chart
+    
+    #Move the chart to the right of the data
     $ws.Shapes.Item("Chart 1").Top = 30
     $ws.Shapes.Item("Chart 1").Left = 350
 
+    #Set the source to the small table we created
     $chart.SetSourceData($ws.Range("f3:g4"))
     $chart.HasTitle = $True
     $chart.ChartTitle.Text = "Hard Disk Allocation"
-    $chart.ApplyLayout(6,69)
+    $chart.ApplyLayout(6,69) #this layout shows numbers on the chart
 
-    #Save the document as an .xlsx with the Full Name of the computer
+    #Save the document as a .xlsx in the same folder as the current script
     $newFullName = $fullName.replace('.csv', '.xlsx')
     $wb.SaveAs($newFullName)
 }
