db14323c979482def2f4c425d94577e58e435795
Carlos <filoxo@users.noreply.github.com>
2014-11-24 09:06:37 -0700

    Removed traces of Script1

--- Script2.ps1
+++ Script2.ps1
@@ -2,55 +2,7 @@
 ## Powershell Administration Script
 ## Group 2B
 ## Carlos Filoteo, Nathan Dudley, Michael Helvey, Travis Selland
-
-<<<<<<< HEAD:531 Project 3 - Powershell Admin Script.ps1
-## ----- Script 1 - Create Active Directory User Accounts
-
-#parameters
-
-param([string]$csv_source="c:\is531\public\GroupProject3Usernames.csv")
-
-# accepts a csv parameter
-if ($csv_source) {
-    $source_exists = Test-Path $csv_source -PathType Leaf
-
-    if($source_exists){
-        $names = Import-Csv $csv_source
-    } else {
-        Write-Host "ERROR: Source ($csv_source) does not exist or is a directory" -ForegroundColor Red
-        break
-    }
-} else {
-    #Should not ever reach this
-    Write-Host "ERROR: Source is not defined" -ForegroundColor Red
-    break
-}
-#loop for each row
-#retrieve params from imported csv
-
-$csv_source
-$names
-
-
-Foreach($line in $names){
-    $username = $line.username
-    $surname = $line.lastname
-    $givenname = $line.firstname
-
-    New-ADUser $username
-    $user = Get-ADUser $username
-    $user.Surname = $surname
-    $user.Givenname = $givenname
-
-    Set-ADUser -instance $user
-}
-
-
-
-
-=======
->>>>>>> b9d15f9fc728a7568b8b3d38ea2733b2a32d3cd8:Script2.ps1
-## ----- Script 2 - Gather information about Windows computers in your enterprise
+## Script 2 - Gather information about Windows computers in your enterprise
 
 ##NEEDSWORK: fix method to get computerName or list of computers.
 param([string]$computer = (Get-WmiObject -Class Win32_Desktop -ComputerName))
